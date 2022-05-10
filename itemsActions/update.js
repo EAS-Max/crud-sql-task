96641a4edcf3985f4eaa6e53fb9f9c19ccd334e4
Max-Barron <max@enterpriseautomation.co.uk>
2022-05-10 16:52:47 +0100

    all done

--- itemsActions/update.js
+++ itemsActions/update.js
@@ -5,7 +5,10 @@ const patch = async (req, res) => {
     console.log(req.body.name)
     try {
         let name = req.body.name
-        let row = await update(req.params.id, req.body.name)
+        let category_id = req.body.category_id;
+        let price = req.body.price;
+        let image_url = req.body.image_url;
+        let row = await update(name, category_id, price, image_url)
         res.json(row)
     } catch (err) {
         res.json({ error: err.message })
